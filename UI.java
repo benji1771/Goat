645b5131d3b2ba3bc37f9683c790afd1b9b797e2
benji1771 <sarolbenjamin_08221999@yahoo.com>
2021-10-04 12:30:56 -0500

    updated ui

--- UI.java
+++ UI.java
@@ -1,5 +1,25 @@
-public class UI{
+import Goat;
+public class UI extends JFrame{
+
+    public UI(){
+        //Start The UI HERE
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setVisible(true);
+
+        //All Components
+        /*
+        Buttons: Keywords____Directory, Create Goat, Burp
+        Textpane: name, max,
+        */
+        JButton userInput = new JButton("Submit");
+        JButton makeGoat = new JButton("Create");
+        JButton burp = new JButton("Burp");
+
+
+        pack();
+    }
     public static void main(String args[]){
-        
+
     }
 }
