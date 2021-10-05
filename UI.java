b4ccdbee8517ac39cf7fd89efa6f1fa929e2933e
benji <sarolbenjamin_08221999@yahoo.com>
2021-10-05 13:55:26 -0500

    More UI stuff

--- UI.java
+++ UI.java
@@ -1,16 +1,27 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.GridBagLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Image;
 
 public class UI extends JFrame implements ActionListener {
     JButton userInput, makeGoat, burp;
     public UI(){
         //Start The UI HERE
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 400);
-        
+        setLocationRelativeTo(null);
 
         //All Components
         /*
@@ -26,10 +37,69 @@ public class UI extends JFrame implements ActionListener {
         burp.addActionListener(this);
 
         JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
 
-        panel.add(userInput);
-        panel.add(burp);
-        panel.add(makeGoat);
+        GridBagConstraints c = new GridBagConstraints();
+
+
+
+        JLabel image = new JLabel();
+        //image.setPreferredSize(new Dimension(200, 200));
+        image.setSize(200, 200);
+
+        ImageIcon img = new ImageIcon("goatImage.jpg");
+        System.out.println(image.getWidth());
+        Image scaled = img.getImage().getScaledInstance(image.getWidth(), image.getHeight(), Image.SCALE_SMOOTH);
+        img = new ImageIcon(scaled);
+        image.setIcon(img);
+        
+
+        image.setIcon(new ImageIcon("goatImage.jpg"));
+        //image.setPreferredSize(new Dimension(200, 200));
+        
+        JTextField pane = new JTextField();
+        pane.setColumns(20);
+        pane.setEditable(false);
+
+        //image
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 2;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        panel.add(image, c);
+
+        c.gridwidth = 1;
+        //Text Field
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 1;
+        panel.add(pane, c);
+
+        //userInput
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 1;
+        panel.add(userInput, c);
+
+        //makeGoat
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 2;
+        panel.add(makeGoat, c);
+
+        //burp
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 3;
+        panel.add(burp, c);
+        
+
+
         this.add(panel);
         pack();
     }
@@ -41,13 +111,13 @@ public class UI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         if(e.getSource() == userInput){
-
+            System.out.println("UserInput Works");
         }
         if(e.getSource() == makeGoat){
-
+            System.out.println("makeGoat Works");
         }
         if(e.getSource() == burp){
-            
+            System.out.println("burp Works");
         }
         
     }
